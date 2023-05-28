58217bf45c3e60bd32806213d777299857068e2e
Ricardo Neves <rmsneves@gmail.com>
2023-05-28 00:50:09 -0400

    new furigana algo

--- userscript.js
+++ userscript.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         WaniKani Item Review Summary
+// @name         DEV WaniKani Item Review Summary
 // @namespace    wanikani
-// @version      0.3
-// @license     GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
+// @version      0.4
+// @license      GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @description  A quick Review item summary plugin that will show the meaning and readings next to the kanji upon the submission of an answer.
 // @author       Ricardo Neves
 // @match        https://www.wanikani.com/subjects/review
@@ -12,11 +12,13 @@
 (function() {
     'use strict';
 
+    const ALWAYS_SHOW_READING = true; // If set to false, don't show readings after meaning quizzes
+
     // Check if WFOF is installed
     if (!window.wkof) {
-    alert('[Your script name here] script requires Wanikani Open Framework.\nYou will now be forwarded to installation instructions.');
-    window.location.href = 'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549';
-    return;
+        alert('[Your script name here] script requires Wanikani Open Framework.\nYou will now be forwarded to installation instructions.');
+        window.location.href = 'https://community.wanikani.com/t/instructions-installing-wanikani-open-framework/28549';
+        return;
     }
 
     let jquery_url = "https://code.jquery.com/jquery-3.7.0.min.js";
@@ -29,12 +31,58 @@
     Promise.all(promises).then(start_script);
 
 
-    function add_furigana(reading, word){
-        const furigana = _.difference(reading.split(""), word.split("")).join("")
-        const kana = reading.replace(furigana, "")
-        const kanji = word.replace(kana,"")
-        const out = word.replace(kanji, "<ruby>"+kanji+"<rt style='font-size: var(--font-size-large);'>"+furigana+"</rt></ruby>")
-        return  out
+    function add_furigana(reading, word, components){
+        console.log(reading, word)
+        let comp_ids=components.join(",")
+
+        var options = {
+            filters: {
+               ids: components
+            }
+        };
+        wkof.Apiv2.fetch_endpoint('subjects', options).then((resp)=>{
+
+            console.log(resp)
+            let kanji_readings = {}
+            resp.data.forEach( (c)=> {
+                kanji_readings[c.data.characters] = c.data.readings.map(x=>x.reading)
+            });
+
+            console.log(kanji_readings)
+
+            const _reading = reading.split("")
+            const _word = word.split("")
+
+            var output = word;
+            var x;
+            for(var i=0, j=0; i<_word.length; i++){
+                const k = _word[i]
+                //console.log("checking", k, _reading[i])
+
+                if(k === _reading[j]) {j++;}
+                else if(k in kanji_readings) {
+                    const _possible_readings = kanji_readings[k]
+                    //console.log(k, _possible_readings)
+                    for(var r of _possible_readings){
+                        //console.log(reading.substring(j), r, reading.substring(j).startsWith(r))
+                        if(reading.substring(j).startsWith(r)){
+                            console.log("found a reading!", r, "for", k)
+                            output = output.replace(k, "<ruby>"+k+"<rt style='font-size: var(--font-size-large);'>"+r+"</rt></ruby>")
+                            j+=r.length;
+                            break;
+                        }
+                    }
+                } else continue;
+            }
+            if(output==word){
+                output = "<ruby>"+word+"<rt style='font-size: var(--font-size-large);'>"+reading+"</rt></ruby>"
+            }
+
+            console.log(output)
+            var kanji_div = $(".character-header__characters")[0]
+            kanji_div.setHTML(output)
+        });
+
     }
 
 
@@ -46,65 +94,69 @@
             let btn = document.getElementsByClassName('quiz-input__submit-button')[0]
             $('.character-header__characters').after('<div id="WKIRS-content-right" class="WKIRS-content-right" style="border-radius: 5px;box-shadow: inset 0 -3px 1px rgba(0,0,0,0.2), inset 0 3px 1px rgba(0,0,0,0);background-color:rgb(255 255 255 / 30%);width: 20em;min-height: 6em; margin: 1em; padding: 0.5em;"></div>');
             $('#WKIRS-content-right').hide()
-           function extract_subject_info(){
-
-               // cleanup
-               if($("#user-response").attr('enabled')==='true'){
-                   //$('#WKIRS-content-left').html("")
-                   $('#WKIRS-content-right').hide()
-                   $('#WKIRS-content-right').html("")
-                   return;
-               }
+            function extract_subject_info(){
 
 
 
-               let subj_url = document.getElementsByClassName("additional-content__item additional-content__item--item-info")[0].getAttribute("href")
-               if(subj_url!=="/subject_info/:id"){
-                   let subj_id = subj_url.replace("/subject_info/","")
-                   wkof.Apiv2.fetch_endpoint('subjects/'+subj_id).then((subj)=>{
 
-                       let meanings=subj['data']['meanings'];
-                       let readings=subj['data']['readings'];
-                       let pos=subj['data']['parts_of_speech'];
-                       let sentences=subj['data']['context_sentences'];
+                let subj_url = document.getElementsByClassName("additional-content__item additional-content__item--item-info")[0].getAttribute("href")
+                if(subj_url!=="/subject_info/:id"){
+                    let subj_id = subj_url.replace("/subject_info/","")
+                    wkof.Apiv2.fetch_endpoint('subjects/'+subj_id).then((subj)=>{
+
+                        // cleanup
+                        if($("#user-response").attr('enabled')==='true'){
+                            //$('#WKIRS-content-left').html("")
+                            $('#WKIRS-content-right').hide()
+                            $('#WKIRS-content-right').html("")
+                            return;
+                        }
 
 
-                       // create content to display
+                        let meanings=subj['data']['meanings'];
+                        let readings=subj['data']['readings'];
+                        let pos=subj['data']['parts_of_speech'];
+                        let sentences=subj['data']['context_sentences'];
+                        let components=subj['data']['component_subject_ids']
 
-                       var content_right = "<p style='font-weight: var(--font-weight-bold); margin-bottom:1em;'>"+meanings.map(s=>s.meaning).join(", ")+"</p>"
-                       // Get readings
-                       if(subj.object==='vocabulary'){
-                           if($(".quiz-input__question-type")[0].innerHTML === "reading"){
-                               var reading = readings.map(s=>s.reading).join("<br>")
-                               var kanji_div = $(".character-header__characters")[0]
-                               var kanji_content = kanji_div.innerHTML
-                               var furigana = add_furigana(reading, kanji_content)
-                               kanji_div.setHTML(furigana)
-                           }
+                        // create content to display
 
-                           content_right += "<p  style='font-size: var(--font-size-small); font-style: italic;'>"+pos.join(", ")+"</p><br>"// + content_right
-                       }
+                        var content_right = "<p style='font-weight: var(--font-weight-bold); margin-bottom:1em;'>"+meanings.map(s=>s.meaning).join(", ")+"</p>"
+                        // Get readings
+                        if(subj.object==='vocabulary'){
+                            if($(".quiz-input__question-type")[0].innerHTML === "reading" || ALWAYS_SHOW_READING){
+                                var reading = readings.map(s=>s.reading).join("<br>")
+                                var kanji_div = $(".character-header__characters")[0]
+                                var kanji_content = kanji_div.innerHTML
+                                add_furigana(reading, kanji_content, components)
+                                //kanji_div.setHTML(furigana)
+                            }
 
-                       if(subj.object==='kanji'){
-                           let _readings = _.groupBy(readings, 'type')
-                           if(_.has(_readings, 'onyomi')){
-                               content_right += "<p> On: "+_readings['onyomi'].map(s=>s.reading).join(', ')+"</p>"
-                           }
-                           if(_.has(_readings, 'kunyomi')){
-                               content_right += "<p style='line-height:var(--spacing-xloose)'> Kun: "+_readings['kunyomi'].map(s=>s.reading).join(', ')+"</p>"
-                           }
-                       }
-                       // insert data into display areas
-                       $('#WKIRS-content-right').html(content_right)
-                       $('#WKIRS-content-right').show()
-                   });
+                            content_right += "<p  style='font-size: var(--font-size-small); font-style: italic;'>"+pos.join(", ")+"</p><br>"// + content_right
+                        }
 
-               }
-           }
+                        if(subj.object==='kanji'){
+                            if($(".quiz-input__question-type")[0].innerHTML === "reading" || ALWAYS_SHOW_READING){
+                                let _readings = _.groupBy(readings, 'type')
+                                if(_.has(_readings, 'onyomi')){
+                                    content_right += "<p> On: "+_readings['onyomi'].map(s=>s.reading).join(', ')+"</p>"
+                                }
+                                if(_.has(_readings, 'kunyomi')){
+                                    content_right += "<p style='line-height:var(--spacing-xloose)'> Kun: "+_readings['kunyomi'].map(s=>s.reading).join(', ')+"</p>"
+                                }
+                            }
+                        }
+                        // insert data into display areas
+                        $('#WKIRS-content-right').html(content_right)
+                        $('#WKIRS-content-right').show()
+                    });
 
-           btn.addEventListener('click', ()=>setTimeout(extract_subject_info, 10))
+                }
+            }
 
-       });
+            btn.addEventListener('click', ()=>setTimeout(extract_subject_info, 10))
+
+        });
 
     };
 
@@ -112,7 +164,7 @@
 
 
 
-   //get subject id
+    //get subject id
 
 
 })();
